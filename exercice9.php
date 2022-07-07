6d86bcc9516de1ee070f7723489be3093cb7f718
jihaneha <98841225+jihaneha@users.noreply.github.com>
2022-07-07 08:50:30 +0200

    Add files via upload

--- exercice9.php
+++ exercice9.php
@@ -6,23 +6,22 @@ include "common/menu.php";
 <main>
     <div class="div">
         <h1 id="h1">Affichage d'une Facture</h1>
-        <form action="exercice9.php" method="GET">
+        <form action="exercice9.php" method="POST">
             <label for="" class="label">Numéro de commande :</label>
             <input type="number" name="num" class="inp">
             <button type="submit" name="submit" class="btn">Envoyer</button>
         </form>
         <?PHP
-        if (isset($_GET['submit'])) {
-            $number = $_GET['num'];
+            if (isset($_POST['submit'])) {
+            $number = $_POST['num'];
             if (empty($number)) {
                 echo " Saisir une valeur dans le champ ci-dessus";
             } else {
-                echo "Facture de la commande de :  $number";
-               
+                echo "Facture de la commande de :  $number";  
             }
-        } else {
+            } else {
             echo "Saisir une valeur dans le champ ci-dessus";
-        }
+            }
         ?>
 
 
